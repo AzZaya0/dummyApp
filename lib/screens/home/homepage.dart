f9cbcc4be581697fc5d0591d74cf2185b6111a2b
AzZaya0 <ajaya.ajyol112@gmail.com>
2023-08-29 10:30:50 +0545

    appbar

--- lib/screens/home/homepage.dart
+++ lib/screens/home/homepage.dart
@@ -9,31 +9,23 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final provs = Provider.of<homePageProvider>(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text("dummy app"),
-        centerTitle: true,
-      ),
-      body: GestureDetector(
-        onTap: () {
-          provs.signout();
-        },
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: Container(
-                decoration:
-                    BoxDecoration(borderRadius: BorderRadius.circular(14)),
-                height: 100,
-                width: 100,
-                alignment: Alignment.center,
-                color: Colors.deepPurpleAccent,
-                child: Text("signout"),
-              ),
-            ),
+        appBar: AppBar(
+          title: Text("dummy app"),
+          centerTitle: true,
+          actions: [
+            IconButton(
+                onPressed: () {
+                  provs.signout();
+                },
+                icon: const Icon(
+                  Icons.logout,
+                  color: Colors.white,
+                  size: 40,
+                ))
           ],
         ),
-      ),
-    );
+        body: Container(
+          child: Text("Welcome back user"),
+        ));
   }
 }
