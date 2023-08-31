0ab45210bff7ac75d8e99e5290c5748863e5c003
AzZaya0 <ajaya.ajyol112@gmail.com>
2023-08-31 09:33:30 +0545

    story

--- lib/screens/home/homepage.dart
+++ lib/screens/home/homepage.dart
@@ -17,46 +17,47 @@ class HomePage extends StatelessWidget {
     final provs = Provider.of<homePageProvider>(context);
     List<UserModel> datalist = [];
     return Scaffold(
+        backgroundColor: kprimaryColor,
         body: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        AppBar(
-          title: Text("dummy app"),
-          backgroundColor: kprimaryColor,
-          actions: [
-            MyIconBtn(
-                ontap: provs.signout, icons: Icons.favorite_border_rounded),
-            Padding(
-              padding: EdgeInsets.only(left: 12),
-              child: MyIconBtn(ontap: provs.signout, icons: Icons.message),
-            )
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AppBar(
+              title: Text("dummy app"),
+              backgroundColor: kprimaryColor,
+              actions: [
+                MyIconBtn(
+                    ontap: provs.signout, icons: Icons.favorite_border_rounded),
+                Padding(
+                  padding: EdgeInsets.only(left: 12),
+                  child: MyIconBtn(ontap: provs.signout, icons: Icons.message),
+                )
+              ],
+            ),
+            StreamBuilder(
+                stream: getallusers(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final data = snapshot.data!.docs;
+                    datalist = data
+                        .map((e) => UserModel.fromJson(
+                            e.data() as Map<String, dynamic>))
+                        .toList();
+                    return _lisBuilder(
+                        datalist.length, datalist, screenwidth, screenhight);
+                  }
+                  return Container(
+                    height: 100,
+                    width: 100,
+                  );
+                })
           ],
-        ),
-        StreamBuilder(
-            stream: getallusers(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                final data = snapshot.data!.docs;
-                datalist = data
-                    .map((e) =>
-                        UserModel.fromJson(e.data() as Map<String, dynamic>))
-                    .toList();
-                return _lisBuilder(
-                    datalist.length, datalist, screenwidth, screenhight);
-              }
-              return Container(
-                height: 100,
-                width: 100,
-              );
-            })
-      ],
-    ));
+        ));
   }
 
   Widget _lisBuilder(
       itemCount, List<UserModel> datalist, screenwidth, screenhight) {
     return Container(
-      height: screenhight * 0.1,
+      height: screenhight * 0.15,
       width: screenwidth,
       child: ListView.builder(
           scrollDirection: Axis.horizontal,
@@ -65,14 +66,24 @@ class HomePage extends StatelessWidget {
             return Padding(
               padding: EdgeInsets.only(
                   right: screenwidth * 0.02, left: screenwidth * 0.02),
-              child: ClipRRect(
-                  borderRadius: BorderRadius.circular(200),
-                  child: Image.network(
-                    (datalist[index]).photoUrl,
-                    height: screenhight * 0.1,
-                    width: screenwidth * 0.2,
-                    fit: BoxFit.fill,
-                  )),
+              child: Column(
+                children: [
+                  ClipRRect(
+                      borderRadius: BorderRadius.circular(200),
+                      child: Image.network(
+                        (datalist[index]).photoUrl,
+                        height: screenhight * 0.1,
+                        width: screenwidth * 0.2,
+                        fit: BoxFit.fill,
+                      )),
+                  Text(
+                    (datalist[index]).username,
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
             );
           }),
     );
