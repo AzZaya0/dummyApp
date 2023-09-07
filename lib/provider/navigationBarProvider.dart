53b37ea7f1a3b66f39fc47704c415498fea32bf1
AzZaya0 <ajaya.ajyol112@gmail.com>
2023-09-07 18:42:41 +0545

    navigtion bar

--- lib/provider/navigationBarProvider.dart
+++ lib/provider/navigationBarProvider.dart
@@ -2,10 +2,30 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyNavigationBarProvider with ChangeNotifier {
-  IconData _home=  Icons.home_outlined;
-  IconData get home=>_home;
+  IconData _home = Icons.home_outlined;
+  IconData get home => _home;
+  int _pageindex = 0;
+  int get pageindex => _pageindex;
 
-  void changeColor( int index) {
-    
+  void changeColor(int index) {
+    switch (index) {
+      case 0:
+        print('zero!');
+        break; // The switch statement must be told to exit, or it will execute every case.
+      case 1:
+        print('one!');
+        break;
+      case 2:
+        print('two!');
+        break;
+      case 3:
+        print('three!');
+        break;
+      case 4:
+        print('four!');
+        break;
+      default:
+        print('choose a different number!');
+    }
   }
 }
